a40117f6f3b0193330558dac4831d3833ec78c71
Brian Su <briansudev@gmail.com>
2013-11-26 00:31:20 -0800

    fixed setup.sh for new dotfiles

--- setup.sh
+++ setup.sh
@@ -38,5 +38,5 @@ mv .bashrc ~/.bashrc
 sudo apt-get install -y zip
 unzip vim_files.zip
 \rm -rf ~/.vim
-mv home/cc/cs61b/fa13/class/cs61b-ba/.vim ~/.vim
+mv .vim ~/.vim
 echo "All files have been successfully initialized." 
